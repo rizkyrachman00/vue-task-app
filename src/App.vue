d9b7c49cb1c63aad2b5e217036acad03655681af
rizkyrachman00 <rachman.ti2021@students.amikom.ac.id>
2025-07-20 16:34:22 +0700

    feat: computed state

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import TaskForm from './components/TaskForm.vue'
 import type { Task } from './types';
 import TaskList from './components/TaskList.vue';
 
 const message = ref('Task App')
 const tasks = ref<Task[]>([])
+
+const totalDone = computed(() => tasks.value.reduce((total, task) => task.done ? total + 1 : total, 0))
 
 function addTask(newTask: string) {
   tasks.value.push({
@@ -31,7 +33,7 @@ function toggleDone(id: string) {
     <h1>{{ message }}</h1>
     <TaskForm @add-task="addTask" />
     <h3 v-if="!tasks.length">Add a task to get started</h3>
-    <h3 v-else="tasks.length">0 / {{ tasks.length }} task completed.</h3>
+    <h3 v-else>{{ totalDone }} / {{ tasks.length }} task completed.</h3>
     <TaskList :tasks @toggle-done="toggleDone" />
   </main>
 </template>
